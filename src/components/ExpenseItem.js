26da8d492e2f8de3f4fda20b950f7d6439ea32b3
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-07-03 23:39:26 +0530

    inital

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <>
-      <div>July 03 2023</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$300.17</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </>
+    </div>
   );
 }
 
