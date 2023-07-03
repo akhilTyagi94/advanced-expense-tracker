1b98f319e623a2a3e7901f653de97a876f09f4af
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-07-03 22:37:20 +0530

    Inital Commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>Amount</div>
+    </>
+  );
+}
+
+export default ExpenseItem;
