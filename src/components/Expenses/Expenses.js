777ab694a51fa94349eeb0b3d45571ec2c811619
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-07-08 13:17:03 +0530

    final commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -21,6 +22,7 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onSelectChange={selectChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
         {/* {filteredExpenses.length === 0 && <p>No Expenses Found.</p>}
         {filteredExpenses.length > 0 &&
